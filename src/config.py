836c84056439ff1a844018f836cd57ba6ab41b7c
RU <110257560+BnanZ0@users.noreply.github.com>
2026-05-25 02:37:03 +0000

    路径B 优化大厅到办公层线路的若干问题 (#108) (Hazuki Kaguya)

--- src/config.py
+++ src/config.py
@@ -6,7 +6,7 @@ from ok import ConfigOption
 from src.interaction.NTEInteraction import NTEInteraction
 from src.process_feature import process_feature
 
-version = "v0.0.43"
+version = "v0.0.44"
 # 不需要修改version, Github Action打包会自动修改
 
 key_config_option = ConfigOption(
