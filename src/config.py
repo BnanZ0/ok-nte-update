4d24e0eaed9fa5a73e98c194484f2f1507dee88b
RU <110257560+BnanZ0@users.noreply.github.com>
2026-07-16 09:33:17 +0000

    feat(task): 重构异象界域任务并优化日常任务配置逻辑 (RU) fix(ui): 增加礼物槽位垂直显示范围 (RU) refactor(daily-task): 简化羁遇赠礼配置项的显示文本 (RU) update i18n (RU) feat(OwnerSelectionTask): 为店长特供任务添加录制与回放功能 (RU) update readme (RU) feat: 添加对 NTETWGame.exe 启动器的支持 (RU)

--- src/config.py
+++ src/config.py
@@ -12,7 +12,7 @@ from src.process_feature import process_feature
 if "PATH" not in os.environ:
     os.environ["PATH"] = ""
 
-version = "v1.2.13"
+version = "v1.2.14"
 # 不需要修改version, Github Action打包会自动修改
 
 key_config_option = ConfigOption(
